bfb0a1a32eefd37adf4a3e1b61f2967df4be40f6
zxi-tech <timotigrandfaldysela@gmail.com>
2026-05-21 20:00:51 +0800

    comments

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,11 +16,11 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        // <-- TAMBAHKAN BARIS INI UNTUK MENDAFTARKAN ALIAS 'admin' -->
+        
         $middleware->alias([
             'admin' => \App\Http\Middleware\CheckAdminRole::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+
     })->create();
