49e3ccb9e61aa9e0608208bebb2d88ddd9f8577a
zxi-tech <timotigrandfaldysela@gmail.com>
2026-08-24 13:29:12 +0800

    feat: add default English translation files for authentication, pagination, passwords, and validation

---
+++ lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Email atau kata sandi yang kamu masukkan tidak sesuai.',
+    'password' => 'Kata sandi yang kamu masukkan salah.',
+    'throttle' => 'Terlalu banyak percobaan masuk. Silakan coba lagi dalam :seconds detik.',
+
+];
